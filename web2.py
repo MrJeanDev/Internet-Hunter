21ff02873bca9a16c7ff40b00ff272cff9b68254
Mr Jean Dev <70442757+MrJeanDev@users.noreply.github.com>
2020-09-01 20:56:22 -0300

    Create web2.py

---
+++ web2.py
@@ -0,0 +1,17 @@
+import socket, whois
+
+def web_service():
+    foda = 11111111111111111111111111111111111111111111
+    print(foda)
+
+def crimeflaredb():
+    foda = 11111111111111111111111111111111111111111111
+    print(foda)
+
+def whois():
+    foda = 11111111111111111111111111111111111111111111
+    print(foda)
+
+def virustotal():
+    foda = 11111111111111111111111111111111111111111111
+    print(foda)

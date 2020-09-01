80f83676c66cafc2219a6633927c61d56604d4df
Mr Jean Dev <70442757+MrJeanDev@users.noreply.github.com>
2020-09-01 20:59:01 -0300

    Update ip2.py

--- ip2.py
+++ ip2.py
@@ -1,25 +1,20 @@
 import socket, nmap3, whois, geoip2, json, scapy
 
 def shodan(api,ip):
-    foda = 11111111111111111111111111111111111111111111
-    print(foda)
+    print()
 
 
 def nmap(ip):
-    foda = 11111111111111111111111111111111111111111111
-    print(foda)
+    print()
 
 
 def traceroute(ip):
-    foda = 11111111111111111111111111111111111111111111
-    print(foda)
+    print()
 
 
 def ping(ip):
-    foda = 11111111111111111111111111111111111111111111
-    print(foda)
+    print()
 
 
 def whois(ip):
-    foda = 11111111111111111111111111111111111111111111
-    print(foda)
+    print()

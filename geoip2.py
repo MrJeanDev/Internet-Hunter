050c52ba1959af79c709047a929e2cbeb72cf158
Mr Jean Dev <70442757+MrJeanDev@users.noreply.github.com>
2020-09-03 14:31:02 -0300

    Update geoip2.py

--- geoip2.py
+++ geoip2.py
@@ -13,10 +13,25 @@ def exec(geoip):
     with geoip2.database.Reader('/path/to/GeoIP2-Domain.mmdb') as reader:
         resposta = leutura.domain(geoip)
         domain = response.domain
-    
+
     with geoip2.database.Reader('/path/to/GeoIP2-Anonymous-IP.mmdb') as reader:
         resposta = reader.anonymous_ip(geoip)
         anonymous = response.is_anonymous
 
 
     return(city, country, anonymous,latitude, longitude, domain, postal_code)
+
+def anonymous(geoip):
+    with geoip2.database.Reader('/path/to/GeoIP2-Anonymous-IP.mmdb') as reader:
+        vpn = response.is_anonymous_vpn
+        hosting = response.is_hosting_provider
+        proxy = response.is_public_proxy
+        tor = response.is_tor_exit_node
+        if vpn == True:
+            return("VPN")
+        elif hosting == True:
+            return("HOSTING")
+        elif proxy == True:
+            return("PROXY")
+        else:
+            return("TOR")

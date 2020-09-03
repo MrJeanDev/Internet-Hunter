85f0146e537cef86aaaa1f9f4dbf40dd9c9fe63d
Mr Jean Dev <70442757+MrJeanDev@users.noreply.github.com>
2020-09-03 14:30:34 -0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,13 @@
+import json
+
+def web(web_service,whois,ip_addr,web_on):
+    print()
+
+def ip(ping ,traceroute,version_result, ports, dns):
+    print()
+
+def geoip(geoip, city, country, anonymous, latitude, longitude, domain, postalcode):
+    print()
+
+def geoip1(geoip, city, country, anonymous, latitude, longitude, domain, postalcode, anonymo):
+    print()

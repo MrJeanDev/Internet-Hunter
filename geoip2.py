b3003072b282d0eaab1370d1dbad8567a68ca5f8
Mr Jean Dev <70442757+MrJeanDev@users.noreply.github.com>
2020-09-06 16:31:44 -0300

    Update geoip2.py

--- geoip2.py
+++ geoip2.py
@@ -1,7 +1,7 @@
-import geoip2
+from geoip2.database import Reader
 
-def exec(geoip):
-    with geoip2.database.Reader('/path/to/GeoLite2-City.mmdb') as reader:
+def city(geoip):
+    with Reader('/path/to/GeoLite2-City.mmdb') as reader:
         response = reader.city(geoip)
         country = response.country.name
         state = response.subdivisions.most_specific.name
@@ -9,20 +9,21 @@ def exec(geoip):
         postal_code = response.postal.code
         latitude = response.location.latitude
         longitude = response.location.longitude
-
-    with geoip2.database.Reader('/path/to/GeoIP2-Domain.mmdb') as reader:
-        resposta = leutura.domain(geoip)
+        return(country,state,city,postal_code,latitude,longitude)
+def domain(geoip):
+    with Reader('/path/to/GeoIP2-Domain.mmdb') as reader:
+        response = reader.domain(geoip)
         domain = response.domain
-
-    with geoip2.database.Reader('/path/to/GeoIP2-Anonymous-IP.mmdb') as reader:
-        resposta = reader.anonymous_ip(geoip)
-        anonymous = response.is_anonymous
-
-
-    return(city, country, anonymous,latitude, longitude, domain, postal_code)
-
+        return(domain)
 def anonymous(geoip):
-    with geoip2.database.Reader('/path/to/GeoIP2-Anonymous-IP.mmdb') as reader:
+    with Reader('/path/to/GeoIP2-Anonymous-IP.mmdb') as reader:
+        response = reader.anonymous_ip(geoip)
+        anonymous = response.is_anonymous
+        return(anonymous)
+
+def anonymous_True(geoip):
+    with Reader('/path/to/GeoIP2-Anonymous-IP.mmdb') as reader:
+        response = reader.anonymous_ip(geoip)
         vpn = response.is_anonymous_vpn
         hosting = response.is_hosting_provider
         proxy = response.is_public_proxy

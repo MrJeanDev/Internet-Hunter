d06d544a7f86a1c76a5ca3bda430aaaad8825741
Mr Jean Dev <70442757+MrJeanDev@users.noreply.github.com>
2020-09-06 16:32:42 -0300

    Update ip2.py

--- ip2.py
+++ ip2.py
@@ -1,5 +1,4 @@
-import socket, nmap3, whois, scapy, time
-
+import socket, nmap3, whois, time
 def dns(ip):
     domain = socket.gethostbyaddr(ip)
     return(domain)
@@ -8,20 +7,9 @@ def nmap(ip):
     nmap = nmap3.Nmap()
     version_result = nmap.nmap_version_detection(ip)
     ports = nmap.scan_top_ports(ip)
-    return(subdomains, version_result, ports)
-
-def traceroute(ip):
-    print()
+    return(version_result, ports)
 
 def whois(ip):
     domain = whois.query(ip)
     whois = domain.__dict__
     return(whois)
-
-def ping(ip):
-    packet = IP(dst=ip + str(ip), ttl=64)
-    reply = sr1(packet)
-    if ip in reply.src:
-         return(True)
-    else:
-        return(False)

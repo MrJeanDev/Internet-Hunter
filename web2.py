428fbf17f56031f83197208eecb729f7f254f39c
Mr Jean Dev <70442757+MrJeanDev@users.noreply.github.com>
2020-09-06 16:33:03 -0300

    Update web2.py

--- web2.py
+++ web2.py
@@ -1,40 +1,39 @@
-import socket, whois, nmap3
-
-def dns(web):
-    ip_addr = socket.gethostbyname(web)
-    subdomains = nmap.nmap_dns_brute_script(ip)
+def dns(target):
+    from socket import gethostbyname
+    from nmap3 import Nmap , NmapScanTechniques
+    nmap = Nmap()
+    ip_addr = gethostbyname(target)
+    subdomains = nmap.nmap_dns_brute_script(target)
     return(ip_addr, subdomains)
 
-def web_on(web):
-    ip = socket.gethostbyname(web), 80
-    try:
-        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        tcp.connect(ip)
-        msg = b"GET / HTTP/1.1\nHost: developer.mozilla.org\n\r\n\r"
-        tcp.send(msg)
-        response = str(tcp.recv(1024))
-        print(oi)
-        if response.find('1.1 404') >= 2:
-            print("Site ou pagina não encontrado\n")
-            result = False
-            return(result)
-        elif response.find('1.1 200') >= 2:
-            print("Site encontrado\n")
-            result = True
-            return(result)
-        else:
-            print("Algo errado na url, mas servidor do site responde normalmente\n")
-            result = True
-            return(result)
-
-    except:
-        print("Deu algo errado, deseja pular? Digita enter.\nSe não. escreve Y e aperte enter")
-def web_service(web):
-    nmap = nmap3.Nmap()
-    version_result = nmap.nmap_version_detection(ip)
+def web_on(target):
+    from socket import gethostbyname, socket
+    ip = gethostbyname(target), 80
+    tcp = socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.connect(ip)
+    msg = b"GET / HTTP/1.1\nHost: developer.mozilla.org\n\r\n\r"
+    tcp.send(msg)
+    response = str(tcp.recv(1024))
+    print(oi)
+    if response.find('1.1 404') >= 2:
+        print("Site ou pagina não encontrado\n")
+        result = False
+        return(result)
+    elif response.find('1.1 200') >= 2:
+        print("Site encontrado\n")
+        result = True
+        return(result)
+    else:
+        print("Algo errado na url, mas servidor do site responde normalmente\n")
+        result = True
+        return(result)
+def web_service(target):
+    from nmap3 import Nmap
+    nmap = Nmap()
+    version_result = nmap.nmap_version_detection(target)
     return(version_result)
 
-def whois(web):
-    domain = whois.query(web)
-    whois = domain.__dict__
-    return(whois)
+def whois(target):
+    from whois import whois
+    whois1 = whois(target)
+    return(whois1)
